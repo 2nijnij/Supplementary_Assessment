ae7643a7e998b259bd3be19a989a06d9c6b1520d
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2024-01-27 19:42:17 +1030

    create attributes
    create attributes in Person class

--- src/ds/graph/Person.java
+++ src/ds/graph/Person.java
@@ -1,8 +1,13 @@
 package ds.graph;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Person {
+	private float socialHygine;
+	private int age;
+	private String name;
+	private ArrayList<Person> contacts;
 	
 	public Person(String name, int age, float socialHygine) {
 		
