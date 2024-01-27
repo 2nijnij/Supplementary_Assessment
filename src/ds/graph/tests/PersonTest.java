1aaccf157709706e8313a8388830998bf507a054
Yejin Kim <kimyy049@mymail.unisa.edu.au>
2024-01-27 21:19:03 +1030

    create PersonTest class

---
+++ src/ds/graph/tests/PersonTest.java
@@ -0,0 +1,10 @@
+package ds.graph.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ds.graph.Person;
+
+public class PersonTest {
+
+}
